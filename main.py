703b5b5a57200cca95b67de15565119620b11ba4
dez1ros <aleksandrv2007@gmail.com>
2026-04-09 19:09:16 +0300

    test fix

--- main.py
+++ main.py
@@ -120,8 +120,8 @@ def form():
                 "template_spec_proh.docx": f"Спецификация {number}.docx",
                 "template_predoplata_proh.docx": f"Счёт предоплата к {number}.docx",
                 "template_postoplata_proh.docx": f"Счёт остаток {number}.docx",
-                "template_KP_proh": f"КП.docx",
-                "template_KO_proh": f"Кассовый ордер {number}.docx"
+                # "template_KP_proh": f"КП.docx",
+                # "template_KO_proh": f"Кассовый ордер {number}.docx"
             }
 
         file_name = f'{number}.zip'
